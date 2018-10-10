b2bb8bd50c555596578ee132b3d12d25e82bf0a2
teraflux <keithklein@outlook.com>
2018-10-10 12:00:22 -0700

    initial commit

---
+++ AWSRegionFinder.ps1
@@ -0,0 +1,42 @@
+﻿function getRemoteIPs($remoteIPs){
+    $remoteIPList=@()
+    foreach($ip in $remoteIPs){
+        if($ip -ne "0.0.0.0" -and $ip -notmatch "\:"){
+            $remoteIPList+=[IPAddress]$ip
+        }
+    }
+    return $remoteIPList
+}
+
+function IPToDecimal($ip){
+    $bytes = $ip.GetAddressBytes()
+    if ([BitConverter]::IsLittleEndian) {
+            [Array]::Reverse($bytes)
+    }
+    [BitConverter]::ToUInt32($bytes, 0)
+}
+
+$AWSEndpointData=@{}
+$AWSEndpoints=((Invoke-WebRequest "https://ip-ranges.amazonaws.com/ip-ranges.json") | ConvertFrom-Json).prefixes
+foreach($AWSEndpoint in $AWSEndpoints){
+    [IPAddress]$ipAddress=$AWSEndpoint.ip_prefix.split("/")[0]
+    [int]$slashNotation=$AWSEndpoint.ip_prefix.split("/")[1]
+    $networkAddresses=[math]::pow(2,32-$slashNotation)
+
+    $AWSEndpointData[$AWSEndpoint.ip_prefix]=@{
+        "StartRange"=(IPToDecimal $ipAddress);
+        "EndRange"=($startDec+$networkAddresses);
+        "AWSRegion"=$AWSEndpoint.region
+    }
+
+}
+
+$RemoteIPList=(Get-NetTCPConnection).RemoteAddress | % {getRemoteIPs $_}
+foreach($remoteIP in $RemoteIPList){
+    $decimalIP=IPToDecimal $remoteIP
+    foreach($AWSRange in $AWSEndpointData){
+        if($decimalIP -gt $AWSRange.StartRange -and $decimalIP -lt $AWSRange.EndRange){
+            Write-Output "$remoteIP - $($AWSRange.EndRange)"
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Project Title
+
+Powershell script that compares the IP addresses your machine is currently connected to against the publicly available AWS regions, so that you can see what region you are connected to. AWS IP ranges pulled from here: 
+https://ip-ranges.amazonaws.com/ip-ranges.json
+
+## Getting Started
+
+Either copy the contents and paste them into a new powershell window, or save the .ps1 file locally and run it within a powershell console.
+
+### Prerequisites
+
+Powershell 5+
+
+## License
+
+This project is licensed under the MIT License
